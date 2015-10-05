f74c73caf7fa59762799736ca52808109301c99b
Jiatao Fan <jiataofan@gmail.com>
2015-10-05 15:31:17 -0400

    copy stencil

---
+++ p_project/3jsbot_matrix.js
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////
+/////     MATRIX ALGEBRA AND GEOMETRIC TRANSFORMS 
+//////////////////////////////////////////////////
+
+/*
+CS148: reference code has functions for:
+
+matrix_multiply
+matrix_transpose
+matrix_pseudoinverse (assuming numeric.inv)
+vector_normalize
+vector_cross
+generate_identity
+generate_translation_matrix
+generate_rotation_matrix_X
+generate_rotation_matrix_Y
+generate_rotation_matrix_Z
+*/
+
+
